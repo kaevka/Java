8c4bac5fbeacd42c2d481beeb9591b564f6d763e
kaevka <105744651+kaevka@users.noreply.github.com>
2022-10-26 19:32:43 +0300

    Add files via upload

---
+++ Movable.java
@@ -0,0 +1,9 @@
+package pr_3_2;
+
+
+public interface Movable {
+    void moveUp ();
+    void moveDown ();
+    void moveLeft ();
+    void moveRight ();
+}
---
+++ MovablePoint.java
@@ -0,0 +1,26 @@
+package pr_3_2;
+
+//import pr_3_2.Movable;
+
+public class MovablePoint implements Movable {
+    protected int x;
+    protected int y;
+    protected int xSpeed;
+    protected int ySpeed;
+    public MovablePoint (int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    @Override
+    public String toString() {return "Point (" + x + ", " + y + ")";}
+    @Override
+    public void moveUp() {y++;}
+    @Override
+    public void moveDown() {y--;}
+    @Override
+    public void moveRight() {x++;}
+    @Override
+    public void moveLeft() {x--;}
+}
